f741baf5d865346e39a6dd462ea3ee1e2fbdd880
Glenn Hartmann <hartmanng@chromium.org>
2021-07-13 10:51:54 -0400

    add nearly-bare Android Studio project and Chromium license, just to be official

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.myapplication;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button httpButton = findViewById(R.id.http_button);
+        httpButton.setOnClickListener(v -> launchUrl("http://slash-statuesque-bracket.glitch.me/pwatest/"));
+
+        final Button httpsButton = findViewById(R.id.https_button);
+        httpsButton.setOnClickListener(v -> launchUrl("https://slash-statuesque-bracket.glitch.me/pwatest/"));
+    }
+
+    private void launchUrl(String url) {
+        try {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+        } catch (ActivityNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
